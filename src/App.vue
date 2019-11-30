bbe691322bc6ae4818059792f8a6c6ddff1c48c6
asanchezaguirre <a.sanchez.aguirre@gmail.com>
2019-11-29 18:36:38 -0600

    add project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>{{ name }}´s awesome to do app!</h1>
+      <form-task @addTask = "addTask"/>
+   
+    <hr />
+    <h2>Pending tasks</h2>
+    <todo-list @toggle = "toggle" :list="pendingTasks"/>
+
+    <hr />
+
+    <h2>Completed tasks</h2>
+     <todo-list @toggle = "toggle" :list="completedTasks"/>
   </div>
+  
+
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
+import todoList from './components/todoList'
+import formTask from './components/formTask'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld,
+  components:{
+    todoList,
+    formTask
   },
-};
-</script>
+  data(){
+    return{
+      name: "Adriana",
+      newTask: "",
+      list: [
+        {label: "Learn Vue", done: true},
+        {label: 'Create To do', done: false},
+        {label: 'Deploy', done: false},
+        {label: 'Go for a drink', done: false},
+        {label: 'Learn', done: false},
+      ]
+    };
+  },
+  methods:{
+    addTask(newTask){
+      if (!newTask){
+        return;
+      }
+      this.list.push({ label: newTask, done:false});
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    },
+    toggle (item){
+      item.done = !item.done;
+    }
+  },
+  computed: {
+    completedTasks(){
+      return this.list.filter(item => item.done)
+    },
+    pendingTasks(){
+      return this.list.filter(item => !item.done)
+    }
+  }
+  
+};
+
+</script>
---
+++ src/components/todoList.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul class="list-group">
+      <li 
+        v-for="(item, index) in list" 
+        v-bind:key= "index"
+        class="list-group-item">
+        {{ item.label }} - {{ item.done ? "completed" : "pending"}}
+        <button 
+          v-on:click="toggle(item)"
+          class= "btn btn-sm btn-primary float-right">toggle
+        </button>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props:{
+        list:{
+            type: Array,
+            default: () => []
+        }
+    },
+    methods:{
+        toggle(item){
+            this.$emit('toggle', item);
+        }
+
+    }
+}
+</script>
